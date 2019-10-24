9118fa83a9ee89f845fa00d93aae7cec4a1199e9
zJtmoon <42380470+zJtmoon@users.noreply.github.com>
2019-10-24 18:45:45 +0800

    Add files via upload
    filedownload

---
+++ FileDownloadUtil.java
@@ -0,0 +1,25 @@
+package com.creditease.honeybot.utils;
+
+import java.io.OutputStream;
+
+import javax.servlet.http.HttpServletResponse;
+
+public class FileDownloadUtil {
+	
+	private FileDownloadUtil() {}
+
+    public static void download(String fileName, byte[] data, HttpServletResponse response) {
+        try (OutputStream out = response.getOutputStream();){
+        	response.setContentType("application/octet-stream");
+        	response.setHeader("Content-Length", String.valueOf(data.length)); 
+        	response.setHeader("Content-disposition", "attachment; filename="
+        			+ new String(fileName.getBytes("utf-8"), "ISO8859-1"));
+        	
+			out.write(data);
+			out.flush();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		} 
+    }
+	
+}
